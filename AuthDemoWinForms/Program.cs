5c50a99ac06110e3f895c13e6f289967ac974abe
Chuck LU <chucklu2015@outlook.com>
2023-02-27 17:25:48 +0800

    handle exception globally

--- AuthDemoWinForms/Program.cs
+++ AuthDemoWinForms/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Identity.Client;
 using System;
+using System.Configuration;
 using System.Windows.Forms;
 
 namespace AuthDemoWinForms
@@ -7,8 +8,8 @@ namespace AuthDemoWinForms
     static class Program
     {
 
-        public static string ClientId = Environment.GetEnvironmentVariable("ClientId", EnvironmentVariableTarget.User);
-        public static string Tenant = Environment.GetEnvironmentVariable("Tenant", EnvironmentVariableTarget.User);
+        public static string ClientId = "";
+        public static string TenantId= "";
         private static IPublicClientApplication clientApp;
 
         /// <summary>
@@ -18,20 +19,38 @@ namespace AuthDemoWinForms
         static void Main()
         {
             InitializeAuth();
+
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+            Application.ThreadException += Application_ThreadException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.ToString());
+        }
+
+        private static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            MessageBox.Show(e.ToString());
         }
 
         public static IPublicClientApplication PublicClientApp { get { return clientApp; } }
 
         private static void InitializeAuth()
         {
+            ClientId= ConfigurationManager.AppSettings[nameof(ClientId)];
+            TenantId= ConfigurationManager.AppSettings[nameof(TenantId)];
 
             clientApp = PublicClientApplicationBuilder.Create(ClientId)
                     .WithRedirectUri("https://login.microsoftonline.com/common/oauth2/nativeclient")
-                    .WithAuthority(AzureCloudInstance.AzurePublic, Tenant)
+                    .WithAuthority(AzureCloudInstance.AzurePublic, TenantId)
                     .Build();
+
             TokenCacheHelper.EnableSerialization(clientApp.UserTokenCache);
         }
     }
